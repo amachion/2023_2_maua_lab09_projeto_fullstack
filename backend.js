34abd0abe19691a1ac415afcf3d072c7d22e3a8b
Andreia Machion <amachion@gmail.com>
2023-11-16 13:37:37 -0300

    aula 16/11 até código 2.21.6

--- backend.js
+++ backend.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt');
 const app = express();
 app.use (express.json());
 app.use (cors());
 
-let filmes = [
-    {
-    titulo: "Forrest Gump - O Contador de Histórias",
-    sinopse: "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks),um rapaz com QI abaixo da média e boas intenções."
-    },
-    {
-    titulo: "Um Sonho de Liberdade",
-    sinopse: "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela"
-    }
-]
-
 async function conectarMongoDB() {
-    await mongoose.connect(`mongodb+srv://pro_mac:mongo123@cluster0.skf8n.mongodb.net/?retryWrites=true&w=majority`);
+    await mongoose.connect(`mongodb+srv://pro_mac:mongo1234@cluster0.skf8n.mongodb.net/?retryWrites=true&w=majority`);
 }
-
 const Filme = mongoose.model ("Filme", mongoose.Schema({
     titulo: {type: String},
     sinopse: {type: String}
 }))
+const usuarioSchema = mongoose.Schema ({
+    login: {type: String, unique: true, required: true},
+    senha: {type: String, required: true}
+})
+usuarioSchema.plugin(uniqueValidator);
+const Usuario = mongoose.model ("Usuario", usuarioSchema);
 
 //acesso para requisição http-get /oi
 app.get('/oi', (req, res) => {res.send('oi')});
@@ -47,6 +43,21 @@ app.post ('/filmes', async (req, res) => {
     const filmes = await Filme.find();
     res.json(filmes);
 });
+app.post ('/signup', async (req, res) => {
+    try {
+        const login = req.body.login;
+        const senha = req.body.senha;
+        const criptografada = await bcrypt.hash(senha, 2);
+        const usuario = new Usuario({login: login, senha: criptografada});
+        const respostaMongo = await usuario.save();
+        console.log (respostaMongo);
+        res.status(201).end();
+    }
+    catch (e) {
+        console.log(e);
+        res.status(409).end();
+    }
+})
 
 app.listen (3000, () => {
     try {
